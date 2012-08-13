170a6ef2cd14c988769071d58bae4950e8bd9033
John Ankcorn <jca@alum.mit.edu>
2012-08-13 17:58:00 -0400

    added zip_files.sh to shift away from patches for 'ignore files'

---
+++ scripts/4.0.4_r1.2/zip_files.sh
@@ -0,0 +1,13 @@
+#
+gzip frameworks/base/core/jni/Android.mk
+gzip frameworks/base/media/jni/Android.mk
+gzip frameworks/base/media/libdrm/mobile1/Android.mk
+gzip frameworks/base/media/libstagefright/chromium_http/Android.mk
+gzip frameworks/base/native/graphics/jni/Android.mk
+gzip frameworks/base/services/jni/Android.mk
+gzip frameworks/base/tests/BrowserTestPlugin/jni/Android.mk
+gzip frameworks/base/tools/layoutlib/Android.mk
+gzip hardware/ril/mock-ril/Android.mk
+gzip system/media/mca/filterfw/Android.mk
+gzip system/media/mca/filterfw/jni/Android.mk
+gzip system/media/mca/filterpacks/imageproc/Android.mk

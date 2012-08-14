8008c7766c9959b2016bd65d1b51f67f6671ab72
John Ankcorn <jca@alum.mit.edu>
2012-08-14 10:46:33 -0400

    changed run.gstripped -> fullbuild

--- scripts/strip-projects.sh
+++ scripts/strip-projects.sh
@@ -1,6 +1,6 @@
 #
 #set -e
 export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-sed -f $SCRIPT_DIR/strip_packages.sed <$1 >xx.strip.tmp
+sed -f $SCRIPT_DIR/sed/strip_packages.sed <$1 >xx.strip.tmp
 [ -e $1.orig ] || mv $1 $1.orig
 mv xx.strip.tmp $1

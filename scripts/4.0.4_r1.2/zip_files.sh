649a63d84195ef14037f71f05479ba7accd6227a
John Ankcorn <jca@alum.mit.edu>
2012-08-16 11:57:24 -0400

    Make the changes to build/core/config.mk with a sed script instead of a patch

--- scripts/4.0.4_r1.2/zip_files.sh
+++ scripts/4.0.4_r1.2/zip_files.sh
@@ -1,16 +1,12 @@
 #
-#gzip frameworks/base/core/jni/Android.mk
-#gzip frameworks/base/media/jni/Android.mk
-#gzip frameworks/base/media/libdrm/mobile1/Android.mk
-#gzip frameworks/base/media/libstagefright/chromium_http/Android.mk
+gzip frameworks/base/core/jni/Android.mk
+gzip frameworks/base/media/jni/Android.mk
+gzip frameworks/base/media/jni/*/Android.mk
 gzip frameworks/base/native/graphics/jni/Android.mk
-#gzip frameworks/base/services/jni/Android.mk
-#gzip frameworks/base/tests/BrowserTestPlugin/jni/Android.mk
+gzip frameworks/base/tests/BrowserTestPlugin/jni/Android.mk
 gzip frameworks/base/tools/layoutlib/Android.mk
 gzip hardware/ril/mock-ril/Android.mk
-#gzip system/media/mca/filterfw/Android.mk
 gzip system/media/mca/filterfw/jni/Android.mk
-#gzip system/media/mca/filterpacks/imageproc/Android.mk
 gzip frameworks/base/libs/rs/Android.mk
 gzip frameworks/base/graphics/jni/Android.mk
 gzip frameworks/base/data/fonts/Android.mk
---
+++ scripts/sed/build_core_config.sed
@@ -0,0 +1,3 @@
+/BUILD_HOST_PREBUILT:=/s/BUILD_SYSTEM)\/host_prebuilt/TOPDIR)dalvik\/null/
+/BUILD_JAVA_LIBRARY:=/s/BUILD_SYSTEM)\/java_library/TOPDIR)dalvik\/null/
+/BUILD_DROIDDOC:=/s/BUILD_SYSTEM)\/droiddoc/TOPDIR)dalvik\/null/

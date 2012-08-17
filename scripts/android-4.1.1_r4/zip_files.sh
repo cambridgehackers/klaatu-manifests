d2947336d87031e3c17f4ca8d0df077a76f47011
John Ankcorn <jca@alum.mit.edu>
2012-08-17 10:57:21 -0400

    added missing commands for 4.1.1

--- scripts/android-4.1.1_r4/zip_files.sh
+++ scripts/android-4.1.1_r4/zip_files.sh
@@ -1,2 +1,8 @@
 #
+gzip frameworks/av/media/libstagefright/chromium_http/Android.mk
+gzip frameworks/wilhelm/tests/native-media/jni/Android.mk
+gzip frameworks/base/media/mca/filterfw/jni/Android.mk
 sed -i.001 -e "/^DEFAULT_HTTP = /s/chrome/notchrome/" frameworks/av/media/libstagefright/Android.mk
+sed -i.001 -e "/^include external\/junit\/Common.mk/d" frameworks/base/Android.mk
+sed -i.001 -e "/^LOCAL_WHOLE_STATIC_LIBRARIES := /s/libfilterfw_jni//" -e "/libjnigraphics/d" frameworks/base/media/mca/filterfw/Android.mk
+sed -i.001 -e "/^ifneq (\$(TARGET_BUILD_PDK), true)/s/\$(TARGET_BUILD_PDK)/true/" frameworks/av/media/libstagefright/Android.mk
--- scripts/zip_files_common.sh
+++ scripts/zip_files_common.sh
@@ -18,7 +18,12 @@ sed -i.001 -e "/^\$(LOCAL_INSTALLED_MODULE):/d" build/core/binary.mk
 sed -i.001 -e "/^include/d" build/core/host_java_library.mk
 sed -i.001 -e "/^include/d" build/core/java.mk
 sed -i.001 -e "/^service zygote/s/^/service powermanager \/system\/bin\/powermanager\n    class main\n    user system\n    group system\n\n/" -e "/^service zygote/,+6d" system/core/rootdir/init.rc
-sed -i.001 -e "/com_android_server_InputManager.cpp/d"  -e "/com_android_server_InputWindowHandle.cpp/d" -e "/onload.cpp/d" frameworks/base/services/jni/Android.mk
+sed -i.001 -e "/com_android_server_InputManager.cpp/d"  -e "/com_android_server_InputWindowHandle.cpp/d" \
+    -e "/onload.cpp/d" -e "/com_android_server_input_InputManagerService.cpp/d" \
+    -e "/com_android_server_input_InputWindowHandle.cpp/d" \
+    -e "/com_android_server_PowerManagerService.cpp/d" -e "/com_android_server_UsbDeviceManager.cpp/d" \
+    -e "/com_android_server_UsbHostManager.cpp/d" -e "/com_android_server_location_GpsLocationProvider.cpp/d" \
+    frameworks/base/services/jni/Android.mk
 sed -i.001 -e "/BUILD_HOST_PREBUILT:=/s/BUILD_SYSTEM)\/host_prebuilt/TOPDIR)dalvik\/null/" \
     -e "/BUILD_JAVA_LIBRARY:=/s/BUILD_SYSTEM)\/java_library/TOPDIR)dalvik\/null/" \
     -e "/BUILD_DROIDDOC:=/s/BUILD_SYSTEM)\/droiddoc/TOPDIR)dalvik\/null/" build/core/config.mk

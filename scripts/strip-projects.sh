6767f3184a6637ca0b32fa2a198e3bd2f3e46c33
John Ankcorn <jca@alum.mit.edu>
2012-08-31 15:17:23 -0400

    the referer repo URL in manifest.xml can just be '..'.
    Remove the hard-coded URL used in qcom and omap manifest files.

--- scripts/strip-projects.sh
+++ scripts/strip-projects.sh
@@ -2,8 +2,4 @@
 #set -e
 export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 [ -e $1.orig ] || cp $1 $1.orig
-sed -i.001 -f $SCRIPT_DIR/sed/strip_packages.sed $1
-REPLSTR=`echo $ANDROID_GENERIC_HOST_CODEAURORA | sed -e "s/\//\\\\\\\\\//g"`
-sed -i.002 -e "s/git:\/\/codeaurora.org/$REPLSTR/" $1
-REPLSTR=`echo $ANDROID_GENERIC_HOST_OMAPZOOM | sed -e "s/\//\\\\\\\\\//g"`
-#sed -i.001 -e "s/git:\/\/git.omapzoom.org/$REPLSTR/" $1
+sed -i.001 -f $SCRIPT_DIR/sed/strip_packages.sed -e "s/git:\/\/codeaurora.org/../" -e "s/git:\/\/git.omapzoom.org/../" $1

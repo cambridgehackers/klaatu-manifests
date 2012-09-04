aefa8dbdbf58a4f3349a3aec8921d134d881a84b
John Ankcorn <jca@alum.mit.edu>
2012-09-04 15:04:20 -0400

    added script for msm8960 4.0.3 build.  This boots to UI and runs correctly

--- scripts/msm8960_4.0.3_full.sh
+++ scripts/msm8960_4.0.3_full.sh
@@ -1,13 +1,13 @@
 #
 set -e
 set -x
-#build for 4.1.1
+#build for 4.0.3
 ANDROID_GENERIC_HOST=${ANDROID_GENERIC_HOST:-https://android.googlesource.com/}
 ANDROID_GENERIC_HOST_CODEAURORA=${ANDROID_GENERIC_HOST_CODEAURORA:-git://codeaurora.org}
 ANDROID_GENERIC_HOST_OMAPZOOM=${ANDROID_GENERIC_HOST_OMAPZOOM:-git://git.omapzoom.org}
 
-repo init -u $ANDROID_GENERIC_HOST_CODEAURORA/platform/manifest -b release -m M8960AAAAANLYA2004.xml 
-ln -sf ~/bionicsf-manifests/manifests/cambridge-vendor-4.1.1.xml .repo/local_manifest.xml
+repo init -u $ANDROID_GENERIC_HOST_CODEAURORA/platform/manifest -b ics_chocolate -m M8960AAAAANLYA1031A.xml 
+ln -sf ~/bionicsf-manifests/manifests/cambridge-vendor-4.0.3.xml .repo/local_manifest.xml
 sed -i.001  -e "s/git:\/\/codeaurora.org/../" -e "s/git:\/\/git.omapzoom.org/../" .repo/manifest.xml 
 repo sync -n -j 4 && repo sync -l -j 20
 ln -sf ../../vendor_extra/qcom/proprietary vendor/qcom/

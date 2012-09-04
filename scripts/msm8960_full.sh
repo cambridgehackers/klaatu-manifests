bfa0dd21322c0b540a7a9c44536ff47650330349
John Ankcorn <jca@alum.mit.edu>
2012-09-04 11:40:46 -0400

    allow re-running script

--- scripts/msm8960_full.sh
+++ scripts/msm8960_full.sh
@@ -7,8 +7,8 @@ ANDROID_GENERIC_HOST_CODEAURORA=${ANDROID_GENERIC_HOST_CODEAURORA:-git://codeaur
 ANDROID_GENERIC_HOST_OMAPZOOM=${ANDROID_GENERIC_HOST_OMAPZOOM:-git://git.omapzoom.org}
 
 repo init -u $ANDROID_GENERIC_HOST_CODEAURORA/platform/manifest -b release -m M8960AAAAANLYA2004.xml 
-ln -s ~/bionicsf-manifests/manifests/cambridge-vendor-4.1.1.xml .repo/local_manifest.xml
+ln -sf ~/bionicsf-manifests/manifests/cambridge-vendor-4.1.1.xml .repo/local_manifest.xml
 sed -i.001  -e "s/git:\/\/codeaurora.org/../" -e "s/git:\/\/git.omapzoom.org/../" .repo/manifest.xml 
 repo sync -n -j 4 && repo sync -l -j 20
-ln -s ../../vendor_extra/qcom/proprietary vendor/qcom/
+ln -sf ../../vendor_extra/qcom/proprietary vendor/qcom/
 source build/envsetup.sh; choosecombo 2 msm8960 eng

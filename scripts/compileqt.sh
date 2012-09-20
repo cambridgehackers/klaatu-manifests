bae80146af3963bc5138ab5de000e203ab1f587d
John Ankcorn <jca@alum.mit.edu>
2012-09-19 23:26:12 -0400

    edited compileqt to reflect gitorious repos

--- scripts/compileqt.sh
+++ scripts/compileqt.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 # ln -s ~/bionicsf-manifests/manifests/cambridge-stripped-4.0.4_r1.2.xml .repo/local_manifest.xml
-rm -rf bionicsf-* loki mysysroot outdir
+rm -rf bionicsf-* loki mysysroot outdir cambridge-qt*
 [ -e mysysroot ] || mkdir mysysroot
 [ -e outdir ] || mkdir outdir
 cd mysysroot
@@ -11,18 +11,15 @@ cd ..
 export KLAATU_SYSROOT=`pwd`/mysysroot/aroot
 INSTALL_TARGET=`pwd`/outdir
 
-#git clone ssh://leda/git/bionicsf-qtbase
 git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtbase.git
-(cd bionicsf-qtbase; git checkout remotes/origin/bionicsf_2012-08-23 -b testing)
-#git clone ssh://leda/git/bionicsf-qtjsbackend
+(cd cambridge-qtbase; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
 git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtjsbackend.git
-(cd bionicsf-qtjsbackend; git checkout remotes/origin/bionicsf_2012-08-23 -b testing)
-#git clone ssh://leda/git/bionicsf-qtdeclarative
+(cd cambridge-qtjsbackend; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
 git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtdeclarative.git
-(cd bionicsf-qtdeclarative; git checkout remotes/origin/bionicsf_2012-08-23 -b testing)
+(cd cambridge-qtdeclarative; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
 git clone ssh://leda/git/loki
 
-cd bionicsf-qtbase
+cd cambridge-qtbase
 ./configure -no-c++11 -no-linuxfb -no-kms \
     	-no-accessibility -opensource -confirm-license \
         -device linux-android-maguro-es-g++ \
@@ -31,11 +28,11 @@ cd bionicsf-qtbase
 make -j32
 INSTALL_ROOT=$INSTALL_TARGET make install
 echo -e "[Paths]\nPrefix=$INSTALL_TARGET/data/usr\nHostData=$INSTALL_TARGET/data/usr" >$INSTALL_TARGET/data/usr/bin/qt.conf
-cd ../bionicsf-qtjsbackend
+cd ../cambridge-qtjsbackend
 $INSTALL_TARGET/data/usr/bin/qmake
 make -j32
 make install
-cd ../bionicsf-qtdeclarative
+cd ../cambridge-qtdeclarative
 $INSTALL_TARGET/data/usr/bin/qmake
 make -j32
 make install

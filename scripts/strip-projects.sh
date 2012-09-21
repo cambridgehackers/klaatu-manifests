e5fb8bdf10935c8c752b1d97d4f3025a61c27ff8
John Ankcorn <jca@alum.mit.edu>
2012-09-20 20:39:29 -0400

    use gitconfig to remap repo names to local mirror
    (so no need to remap them in the manifest.xml file)

--- scripts/strip-projects.sh
+++ scripts/strip-projects.sh
@@ -6,6 +6,4 @@ export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 sed -i.001 -f $SCRIPT_DIR/sed/strip_packages.sed \
     -e "/<\/manifest>/r $SCRIPT_DIR/new_projects.xml" \
     -e "/<\/manifest>/d" \
-    -e "s/git:\/\/codeaurora.org/../" \
-    -e "s/git:\/\/git.omapzoom.org/../" \
     $1

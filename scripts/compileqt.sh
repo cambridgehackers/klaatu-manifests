0c3587d25305cd541d5c12b03bc96543028b10cc
John Ankcorn <jca@alum.mit.edu>
2012-09-27 13:00:08 -0400

    updated to use 'repo' to fetch packages

--- scripts/compileqt.sh
+++ scripts/compileqt.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 set -x
-# ln -s ~/bionicsf-manifests/manifests/cambridge-stripped-4.0.4_r1.2.xml .repo/local_manifest.xml
-rm -rf bionicsf-* loki mysysroot outdir cambridge-qt*
 [ -e mysysroot ] || mkdir mysysroot
 [ -e outdir ] || mkdir outdir
+repo init -u git://gitorious.org/cambridge/klaatu-manifests.git -m manifests/qt_2012-09-12.xml
+
 cd mysysroot
-~/klaatu-manifests/scripts/install_sysroot.sh ~/rpm 4.0.4_r1.2 maguro
+.repo/manifests/scripts/install_sysroot.sh ~/rpm 4.0.4_r1.2 maguro
 cd ..
 export KLAATU_SYSROOT=`pwd`/mysysroot/aroot
 INSTALL_TARGET=`pwd`/outdir
 
-git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtbase.git
-(cd cambridge-qtbase; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
-git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtjsbackend.git
-(cd cambridge-qtjsbackend; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
-git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtdeclarative.git
-(cd cambridge-qtdeclarative; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
-git clone ssh://leda/git/loki
+#git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtbase.git
+#(cd cambridge-qtbase; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
+#git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtjsbackend.git
+#(cd cambridge-qtjsbackend; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
+#git clone git://gitorious.org/+cambridgehackers/qt/cambridge-qtdeclarative.git
+#(cd cambridge-qtdeclarative; git checkout remotes/origin/bionicsf_2012-09-12 -b testing)
+#git clone ssh://leda/git/loki
 
-cd cambridge-qtbase
+cd qtbase
 ./configure -no-c++11 -no-linuxfb -no-kms \
     	-no-accessibility -opensource -confirm-license \
         -device linux-android-maguro-es-g++-klaatu \
@@ -28,11 +28,11 @@ cd cambridge-qtbase
 make -j32
 INSTALL_ROOT=$INSTALL_TARGET make install
 echo -e "[Paths]\nPrefix=$INSTALL_TARGET/data/usr\nHostData=$INSTALL_TARGET/data/usr" >$INSTALL_TARGET/data/usr/bin/qt.conf
-cd ../cambridge-qtjsbackend
+cd ../qtjsbackend
 $INSTALL_TARGET/data/usr/bin/qmake
 make -j32
 make install
-cd ../cambridge-qtdeclarative
+cd ../qtdeclarative
 $INSTALL_TARGET/data/usr/bin/qmake
 make -j32
 make install

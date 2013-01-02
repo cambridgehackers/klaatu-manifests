b0777481f9b2886a3fc6f36212df1f36e1d591ef
Don Mahurin <@>
2013-01-02 09:42:22 -0800

    move success/fail messages to end.

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 if [ -z "$1" ]; then
         echo "usage $0 BUILD_SCRIPT BUILD_DIR OUT_DIR"
@@ -8,16 +8,16 @@ fi
 script=$1
 shift
 
-build_dir=/ramdisk/jenkins_builds/$JOB_NAME/$BUILD_ID
+build_dir="/ramdisk/jenkins_builds/${JOB_NAME}_${BUILD_ID}"
 
 echo "Build start at $(date --rfc-3339=seconds)"
 if ( "$(dirname $0)/$script" $build_dir ); then
+	mkdir -p "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"
+	cp -a $build_dir/out "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"
+	rm -rf "$build_dir"
 	echo "Build success at $(date --rfc-3339=seconds)"
-	mkdir -p /home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID
-	cp -a $build_dir/out /home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID
-	rm -rf $build_dir
 else
+	rm -rf "$build_dir"
 	echo "Build fail at $(date --rfc-3339=seconds)"
-	rm -rf $build_dir
 	false
 fi

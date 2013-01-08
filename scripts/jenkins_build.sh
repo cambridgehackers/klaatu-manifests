8e3ce1706d71a8ef05b7771e1c437bca7b74515b
Don Mahurin <@>
2013-01-07 20:23:24 -0800

    exclude obj in copy

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -13,7 +13,7 @@ build_dir="/ramdisk/jenkins_builds/${JOB_NAME}_${BUILD_ID}"
 echo "Build start at $(date --rfc-3339=seconds)"
 if ( "$(dirname $0)/$script" $build_dir ); then
 	mkdir -p "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"/out
-	cp -a $build_dir/out/* "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"/out
+	rsync -a --exclude obj $build_dir/out/ "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"/out/
 	rm -rf "$build_dir"
 	echo "Build success at $(date --rfc-3339=seconds)"
 else

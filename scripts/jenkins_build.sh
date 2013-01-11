b00a48d59702fb0c2b5fbf16f79ab6edcfecf514
Don Mahurin <@>
2013-01-11 08:48:56 -0800

    use bash

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -1,17 +1,18 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 if [ -z "$1" ]; then
         echo "usage $0 BUILD_SCRIPT BUILD_DIR OUT_DIR"
         exit 1
 fi
 
-script=$1
+script="$(dirname $0)/$1"
 shift
 
 build_dir="/ramdisk/jenkins_builds/${JOB_NAME}_${BUILD_ID}"
 
 echo "Build start at $(date --rfc-3339=seconds)"
-if ( "$(dirname $0)/$script" $build_dir ); then
+
+if ( "$script" $build_dir ); then
 	mkdir -p "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"/out
 	rsync -a --exclude obj $build_dir/out/ "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"/out/
 	rm -rf "$build_dir"

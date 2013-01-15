a96adbbe86fa365deb20b11ef57b3b393c1c8d76
Don Mahurin <@>
2013-01-15 14:57:10 -0800

    remove build dir unless flagged to keep

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 if [ -z "$1" ] || [ -z "$WORKSPACE" ]; then
         echo "usage $0 BUILD_SCRIPT BUILD_DIR OUT_DIR"
@@ -12,23 +12,31 @@ build_dir="/ramdisk/jenkins_builds/${JOB_NAME}_${BUILD_ID}"
 
 echo "Build start at $(date --rfc-3339=seconds)"
 
+fail=0
+
 rm -f $WORKSPACE/*
 if ( "$script" $build_dir ); then
+	echo "Build success at $(date --rfc-3339=seconds)"
 	out_dir=`find "$build_dir/out/" -name userdata.img`
 	if [ -n "$out_dir" ]; then
 		out_dir=`dirname "$out_dir"`
 		cp -a "$out_dir"/*.*  $WORKSPACE
-		tar -zcvf $WORKSPACE/system.tar.gz -C "$out_dir" system
-		tar -zcvf $WORKSPACE/root.tar.gz -C "$out_dir" root
+		tar -zcf $WORKSPACE/system.tar.gz -C "$out_dir" system
+		tar -zcf $WORKSPACE/root.tar.gz -C "$out_dir" root
 	else
 		echo "error userdata.img not found"
-		mkdir -p "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"/out
-		rsync -a --exclude obj $build_dir/out/ "/home/builds/jenkins_builds/$JOB_NAME/$BUILD_ID"/out/
+		fail=1
 	fi
-	rm -rf "$build_dir"
-	echo "Build success at $(date --rfc-3339=seconds)"
+else
+	echo "Build fail at $(date --rfc-3339=seconds)"
+	fail=1
+fi
+
+job_dir="$HOME/jobs/$JOB_NAME/builds/$BUILD_ID"
+if [ -f "$build_dir"/.keep ] || ( grep keep "$job_dir"/build.xml | grep -q true ); then
+	echo "keeping build directory"
 else
 	rm -rf "$build_dir"
-	echo "Build fail at $(date --rfc-3339=seconds)"
-	false
 fi
+
+exit $fail

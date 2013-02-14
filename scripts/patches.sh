59951aacd238f4de43f8c474916fbeee21ff9788
Greg Bentley <greg.bentley@nokia.com>
2013-02-14 00:34:51 -0700

    Moved common code into its own file.

---
+++ scripts/patches.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Apply any patches that are applicable to this build
+patch_build()
+{
+  if [ -z "$PATCH_DIR" ] ; then PATCH_DIR=`dirname $0`/../patches ; fi
+
+  #build/target specific patches
+  if [ -f "${PATCH_DIR}/${build_name}.series" ]; then
+    cat "${PATCH_DIR}/${build_name}.series" | sed "s:^:"${PATCH_DIR}"/:g" | xargs -L1 patch -p0 -i
+  elif [ -f "${PATCH_DIR}/${build_name}.patch" ]; then
+    patch -p0 < "${PATCH_DIR}/${build_name}.patch"
+  fi
+}

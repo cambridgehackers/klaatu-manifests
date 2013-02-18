00c65712e451c354034e4a4ed636dd8c042ccf54
Don Mahurin <@>
2013-02-18 14:39:00 -0800

    define build_name from build script name

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -5,6 +5,8 @@ patch_build()
 {
   if [ -z "$PATCH_DIR" ] ; then PATCH_DIR=`dirname $0`/../patches ; fi
 
+  build_name="$(basename $0 .sh | sed 's:.*/::g' | sed 's:^build_::g' | sed 's:_:-:g' )"
+
   #build/target specific patches
   if [ -f "${PATCH_DIR}/${build_name}.series" ]; then
     cat "${PATCH_DIR}/${build_name}.series" | sed "s:^:"${PATCH_DIR}"/:g" | xargs -L1 patch -p0 -i

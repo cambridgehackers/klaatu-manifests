5f2088382899429123c75ca34b9bc482144d55a7
Don Mahurin <@>
2013-10-24 09:55:33 -0700

    add strip_projects/fixup_common, for minimal build

--- scripts/build_maguro_klaatu.sh
+++ scripts/build_maguro_klaatu.sh
@@ -9,12 +9,15 @@ vendor_xml=`$script_dir/get_vendor_build.sh $vendor_build`
 klaatu_manifests=$script_dir/../manifests
 
 repo init -u https://android.googlesource.com/platform/manifest -b android-$release
+$script_dir/strip-projects.sh .repo/manifest.xml
+
 mkdir -p .repo/local_manifests
 cp $vendor_xml .repo/local_manifests/
 cp $klaatu_manifests/klaatu-common.xml .repo/local_manifests/
 cp $klaatu_manifests/busybox.xml .repo/local_manifests/
 
 repo sync
+$script_dir/fixup_common.sh
 
 . build/envsetup.sh
 lunch full_maguro-userdebug

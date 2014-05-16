27a6aa40a7628dfc856fcc5940ebd4ccc70102a2
Greg Bentley <greg.bentley@nokia.com>
2014-05-15 22:01:45 -0700

    Added --default-ui command line option.

--- scripts/multi_manifests.sh
+++ scripts/multi_manifests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+let i=1
 for arg in "$@"; do
+  let i=$i+1
+  if [ "$skip_next" == "true" ]; then
+    skip_next=false
+    continue
+  fi
+
   include_arg=`echo $arg | grep "^--include" | sed "s/--include-\(.*\)/KLAATU_INCLUDE_\1/" | tr [:lower:] [:upper:]`
   if [ "$include_arg" != "" ]; then
     export $include_arg=true
@@ -10,12 +17,21 @@ for arg in "$@"; do
   if [ "$exclude_arg" != "" ]; then
     export $exclude_arg=false
   fi
+
+  if [ "$arg" == "--default-ui" ]; then
+    if [ $i -gt $# ]; then
+      echo Missing parameter to --default-ui
+      exit -1
+    fi
+    export KLAATU_DEFAULT_UI=${!i}
+    skip_next=true
+  fi
 done
 
 copy_manifests()
 {
   mkdir -p .repo/local_manifests
-  KLAATU_COMPONENTS=${!KLAATU_INCLUDE_*}
+  export KLAATU_COMPONENTS=${!KLAATU_INCLUDE_*}
   for arg in $KLAATU_COMPONENTS; do
     if [ `printenv $arg` == true ] ; then
       XML_FILE=`echo $arg | tr [:upper:] [:lower:] | sed "s:klaatu_include_\(.*\):../manifest/manifests/klaatu-\1.xml:"`

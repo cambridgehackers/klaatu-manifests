d20ab4fcdd2740112e0aa7dfc9ecb19c5d66d61d
Don Mahurin <@>
2014-05-28 14:15:47 -0700

    klaatu build for deb/razorg

---
+++ scripts/build_deb_klaatu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xel
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+. $script_dir/repo_mirror.sh
+
+release=4.4.2_r1
+vendor_build=deb-kot49h
+vendor_xml=`$script_dir/get_vendor_build.sh $vendor_build`
+klaatu_manifests=$script_dir/../manifests
+
+repo init -u https://android.googlesource.com/platform/manifest -b android-$release
+#$script_dir/strip-projects.sh .repo/manifest.xml
+
+mkdir -p .repo/local_manifests
+cp $vendor_xml .repo/local_manifests/
+cp $klaatu_manifests/klaatu-common.xml .repo/local_manifests/
+cp $klaatu_manifests/busybox.xml .repo/local_manifests/
+cp $klaatu_manifests/klaatu-qt.xml .repo/local_manifests/
+
+repo sync
+$script_dir/fixup_common.sh
+
+export KLAATU_DEFAULT_UI=qt
+
+. build/envsetup.sh
+lunch aosp_deb-userdebug
+
+make -j$NUM_CPUS

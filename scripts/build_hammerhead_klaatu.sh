d09ddb97b984e5f96353cc542cb5528659d19a23
Dmitriy Cherkasov <@>
2014-07-11 15:39:06 -0700

    build_hammerhead: build kernel from scratch, pass thru repo args

--- scripts/build_hammerhead_klaatu.sh
+++ scripts/build_hammerhead_klaatu.sh
@@ -2,6 +2,7 @@
 
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 . $script_dir/repo_mirror.sh
+. $script_dir/patches.sh
 . $script_dir/ndk_setup.sh
 . $script_dir/multi_manifests.sh
 
@@ -11,17 +12,22 @@ release=4.4.4_r1
 vendor_build=hammerhead-ktu84p
 vendor_xml=`$script_dir/get_vendor_build.sh $vendor_build`
 
-repo init -u https://android.googlesource.com/platform/manifest -b android-$release
-#$script_dir/strip-projects.sh .repo/manifest.xml
+repo init -u https://android.googlesource.com/platform/manifest -b android-$release "$@"
 
 set_ui_defaults kivy qt busybox
 
 mkdir -p .repo/local_manifests
 cp $vendor_xml .repo/local_manifests/
+cat <<EOF >.repo/local_manifests/local_manifest.xml
+<manifest>
+  <project path="kernel" name="kernel/msm" revision="android-msm-hammerhead-3.4-kitkat-mr2" />
+</manifest>
+EOF
 manifests="$(get_manifests)"
 if [ -n "$manifests" ]; then cp $manifests .repo/local_manifests/; fi
 
-repo sync
+
+repo sync "$@"
 
 if [ -n "$manifests" ]; then
 $script_dir/fixup_common.sh
@@ -29,7 +35,10 @@ fi
 
 export KLAATU_DEFAULT_UI=$(get_default_ui)
 
+patch_build
+
 . build/envsetup.sh
+
 lunch aosp_hammerhead-userdebug
 
-make -j$NUM_CPUS
+make -j${NUM_CPUS}
